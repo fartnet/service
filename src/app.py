ea306124a94ae6bf4086e44a2e2fe068eb88bdd2
Will Nitsch <will.nitsch@intentsg.com>
2019-11-24 15:18:03 -0500

    made it oop

--- src/app.py
+++ src/app.py
@@ -1,27 +1,30 @@
 from flask import Flask
 from config import *
 import tensorflow as tf
+import numpy as np
 import os
 
 app = Flask(__name__)
 
-# Load the graph
-tf.reset_default_graph()
-saver = tf.train.import_meta_graph(MODEL_WEIGHTS_META)
-graph = tf.get_default_graph()
-sess = tf.InteractiveSession()
-saver.restore(sess, MODEL_WEIGHTS_CKPT)
+class fartNet:
+    def __init__():
+        # Load the graph
+        tf.reset_default_graph()
+        self.saver = tf.train.import_meta_graph(MODEL_WEIGHTS_META)
+        self.graph = tf.get_default_graph()
+        self.sess = tf.InteractiveSession()
+        self.saver.restore(sess, MODEL_WEIGHTS_CKPT)
 
-def predict():
-    # Create 50 random latent vectors z
-    _z = (np.random.rand(50, 100) * 2.) - 1
+    def predict(self):
+        # Create 50 random latent vectors z
+        _z = (np.random.rand(50, 100) * 2.) - 1
 
-    # Synthesize G(z)
-    z = graph.get_tensor_by_name('z:0')
-    G_z = graph.get_tensor_by_name('G_z:0')
-    fart = sess.run(G_z, {z: _z})
-    
-    return fart
+        # Synthesize G(z)
+        z = self.graph.get_tensor_by_name('z:0')
+        G_z = self.graph.get_tensor_by_name('G_z:0')
+        fart = self.sess.run(G_z, {z: _z})
+        
+        return fart
 
 @app.route("/")
 def hello():
