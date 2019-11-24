f1febe8d92d8c6f54a4e15c18ee3a7d65d48b45d
Will Nitsch <will.nitsch@intentsg.com>
2019-11-24 15:27:49 -0500

    made it so the weights can be grabbed from s3 via boto3

--- src/app.py
+++ src/app.py
@@ -3,8 +3,15 @@ from config import *
 import tensorflow as tf
 import numpy as np
 import os
+import boto3
 
 app = Flask(__name__)
+
+# Get the weights from s3 so we can update them on the
+# fly and just reboot the server
+s3 = boto3.resource('s3')
+s3.Bucket(S3_BUCKET).download_file(S3_OBJ_META, MODEL_WEIGHTS_META)
+s3.Bucket(S3_BUCKET).download_file(S3_OBJ_CKPT, MODEL_WEIGHTS_CKPT)
 
 class fartNet:
     def __init__():
--- src/config.py
+++ src/config.py
@@ -1,2 +1,5 @@
 MODEL_WEIGHTS_META = os.path.expanduser('~/fartnet_weights/infer.meta')
 MODEL_WEIGHTS_CKPT = os.path.expanduser('~/fartnet_weights/model.ckpt')
+S3_BUCKET = 'fartnet'
+S3_OBJ_META = 'infer.meta'
+S3_OBJ_CKPT = 'weights.ckpt'
